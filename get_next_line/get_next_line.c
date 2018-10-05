d3128e8312e1c24801912cb9fbc504d6e104a118
Shana COHEN <shcohen@e3r8p10.42.fr>
2018-10-05 17:23:23 +0200

    update october 2018

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,16 @@
 /*   By: shcohen <shcohen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 18:19:06 by shcohen           #+#    #+#             */
-/*   Updated: 2018/10/04 18:58:10 by shcohen          ###   ########.fr       */
+/*   Updated: 2018/10/05 17:21:28 by shcohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_read_line(const int fd, char *save, char *tmp)
+char	*ft_read_line(const int fd, char *save)
 {
 	char	buf[BUFF_SIZE + 1];
+    char    *tmp;
 	int		lus; // caractères lus
 
 	while ((lus = read(fd, buf, BUFF_SIZE)) > 0)
@@ -32,37 +33,42 @@ char	*ft_read_line(const int fd, char *save, char *tmp)
 	return (save);
 }
 
+int    ft_fill_line(char **line, char **save, int i)
+{
+    char    *tmp;
+
+    if ((*save)[i] != '\n')
+		i++;
+	else if ((*save)[i] == '\n')
+	{
+		*line = ft_strsub(*save, 0, i);
+		tmp = *save; // leaks
+		*save = ft_strsub(*save, i + 1, ft_strlen(*save));
+		if (tmp)
+			free(tmp); // leaks
+        return (-1);
+	}
+    return (i);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static char	*save = 0; // récupération des éléments de lecture
-	char		*tmp;
 	int			i;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || BUFF_SIZE < 0)
 		return (-1);
-	tmp = NULL;
 	i = 0;
 	if (!save)
 		save = ft_strdup(""); // 1er appel de save (save = vide)
-	if (!ft_strchr(save, '\n'))
-		save = ft_read_line(fd, save, tmp);
+	if (!ft_strchr(save, '\n')) // = si pas \n alors retourner lire
+		save = ft_read_line(fd, save);
 	if (!save)
-		return (-1);
+		return (-1); // si lecture finie
 	while (save && save[i])
-	{
-		if (save[i] != '\n')
-			i++;
-		else if (save[i] == '\n')
-		{
-			*line = ft_strsub(save, 0, i);
-			tmp = save; // leaks
-			save = ft_strsub(save, i + 1, ft_strlen(save));
-			if (tmp)
-				free(tmp); // leaks
-			return (1);
-		}
-	}
+        if ((i = ft_fill_line(line, &save, i)) == -1)
+            return (1);
 	*line = save;
 	save = 0;
-	return (!(!i));
+	return (!(!i)); // = 1 ou 0;
 }
