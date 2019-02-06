575cf0535e8a56cd81c1a98e5d2046f5fe946f46
Shana COHEN <shcohen@e3r5p8.42.fr>
2019-02-06 13:47:20 +0100

    final update

---
+++ Camagru/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Camagru</title>
+    <link rel="icon" href="assets/icon.png">
+    <link rel="stylesheet" href="style/index.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="start"><p class="title">CAMAGRU</p></div>
+<div class="navbar"><ul>
+    <li class="dropdown"><a class="dropbtn" href="index.php"><i class="fa fa-fw fa-home"></i>HOME</a></li>
+    <li class="dropdown"><a class="dropbtn" href="#"><i class="fa fa-fw fa-search"></i>SEARCH</a></li>
+    <li class="dropdown"><a class="dropbtn" href="login.php"><i class="fa fa-fw fa-user"></i>USER</a></li>
+    <li class="dropdown"><a class="dropbtn" href="contact.php"><i class="fa fa-fw fa-envelope"></i>CONTACT</a></li>
+</ul></div>
+</body>
+</html>
